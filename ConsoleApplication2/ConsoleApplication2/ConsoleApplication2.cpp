e77653c48bd507be737305d726a6ffc561bf6160
ehtasham-jutt69 <150357962+ehtasham-jutt69@users.noreply.github.com>
2025-05-07 23:49:04 +0500

    getter and setter

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class percentage
+{
+private:
+
+    int marks, total;
+
+
+public:
+
+    void getmarks()
+    {
+        return marks;
+    }
+    void gettotal()
+    {
+        return total;
+    }
+    void set(int m, int t)
+    {
+        marks = m;
+        total = t
+    }
+    void perce(m, t)
+    {
+
+        double p = m * 100 / t;
+        return p;
+    }
+
+}
+int main()
+{
+    int marks, total;
+    double p;
+    getmarks();
+    gettotal();
+    percentage(marks, total);
+    set(marks, total);
+    perce(marks, total)
+}
