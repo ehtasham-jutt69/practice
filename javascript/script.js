93f63508b723bb9d6ef1d51667901eb1e18cffb2
ehtasham-jutt69 <150357962+ehtasham-jutt69@users.noreply.github.com>
2025-05-08 03:19:50 +0500

    primitive |non-primative datatype

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+let st="aaa";
+// st=st+3;
+// console.log(typeof(st));
+const obj={
+a:12,
+"ali":"saad",
+c:"hkk",
+d:undefined
+
+
+}
+// you can also define an key outside the class
+obj['f']="newww";   
+// you can also reassign a value to key 
+obj.ali=10;
+console.log(obj);
