12ca0b04991b11e6cf155e28554724ff282caa47
ehtasham-jutt69 <150357962+ehtasham-jutt69@users.noreply.github.com>
2025-05-18 23:33:16 +0500

    c++ basics

---
+++ temperature convertor/ConsoleApplication42.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int c, f;
+	cout << " enter temprature in celsius " << endl;
+	cin >> c;
+	f = (c * 9 / 5) + 32;
+	cout << " fehrenheit = " << f;
+
+}
